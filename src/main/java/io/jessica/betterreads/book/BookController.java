b79b3be292643b90580f1a0808f3c16003982f8d
Jessica Kline <kline.jessica@gmail.com>
2021-10-29 15:35:52 -0700

    User Books

--- src/main/java/io/jessica/betterreads/book/BookController.java
+++ src/main/java/io/jessica/betterreads/book/BookController.java
@@ -3,11 +3,18 @@ package io.jessica.betterreads.book;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.result.method.annotation.PrincipalMethodArgumentResolver;
+
+import io.jessica.betterreads.userbooks.UserBooks;
+import io.jessica.betterreads.userbooks.UserBooksPrimaryKey;
+import io.jessica.betterreads.userbooks.UserBooksRepository;
 
 
 @Controller
@@ -18,8 +25,11 @@ public class BookController {
     @Autowired 
     BookRepository bookRepository;
 
+    @Autowired
+    UserBooksRepository userBooksRepository;
+
     @GetMapping(value="/books/{bookId}")
-    public String getBook(@PathVariable String bookId, Model model) {
+    public String getBook(@PathVariable String bookId, Model model, @AuthenticationPrincipal OAuth2User principle) {
         Optional<Book> optionalBook = bookRepository.findById(bookId);
         if(optionalBook.isPresent()){
             Book book = optionalBook.get();
@@ -29,6 +39,21 @@ public class BookController {
             }
             model.addAttribute("coverImg", coverImgURL);
             model.addAttribute("book", book);
+
+            if(principle != null && principle.getAttribute("login") != null){
+                String userId = principle.getAttribute("login");
+                model.addAttribute("loginId", principle.getAttribute("login"));
+                UserBooksPrimaryKey key = new UserBooksPrimaryKey();
+                key.setBookId(bookId);
+                key.setUserId(userId);
+                Optional<UserBooks> userBooks = userBooksRepository.findById(key);
+                if(userBooks.isPresent()){
+                    model.addAttribute("userBooks", userBooks.get());
+                } else {
+                    model.addAttribute("userBooks", new UserBooks());
+                }
+            }
+ 
             return "book";
         }
        return "book-not-found";
